aa8283ef8ad31d0537069401c7402c8f546bb031
Jim Smith <jsbrava@hotmail.com>
2015-01-29 07:20:31 -0600

    basic funtionality, working on look ahead

--- phasing.py
+++ phasing.py
@@ -4,6 +4,7 @@ Created on Jan 12, 2015
 @author: Jim
 '''
 import util
+SNP_LIMIT = 500
 
 def pattern(mom_snp, sib_list):
     '''
@@ -46,24 +47,59 @@ def test_pattern():
     assert pattern('CA', ['CC', 'AC', 'CT', 'TT']) == ('C-C-', 'C-TT')
     assert pattern('CC', ['CC', 'AC', 'CT', 'TT']) == ('CCC-', 'CATT')   
 
-def informative_pattern(pattern):
-    '''
-    take a pattern, and return empty string if not informative, or full string if informative
-    informative are like 'AACC' or 'TCCC'
-    there should only be two letters, but I don't test
-    '''
-    not_informative = ''
-    if '-' in pattern:
-        return not_informative
-    for letter in pattern[1:]:
-        if pattern[0] != letter:
-            return pattern
-    return not_informative
-def test_informative():
-    assert informative_pattern('AACC') == 'AACC'
-    assert informative_pattern('A-CC') == ''
-    assert informative_pattern('CCCC') == ''
-
+#def informative_pattern(pattern, c_pattern=''):
+#    '''
+#    take a pattern, and return empty string if not informative, or full string if informative
+#    c_pattern is the current pattern, like '0111' or '1100'
+#    informative are like 'AACC' or 'TCCC'
+#    there should only be two letters, but I don't test
+#    return '' if no informative pattern, or '0111' or some such informative pattern plus a Hamming distance
+#    if the Hamming distance is 0 or 1, all is well. If greater, then there's a 50/50 chance the resulting pattern is wrong
+#    '''
+#    not_informative = ''
+#    if '-' in pattern:
+#        return not_informative, 0
+#    if c_pattern == '':
+#        first_digit = '0'
+#        second_digit = '1'
+#    else:
+#        first_digit = c_pattern[0]
+#        if first_digit == '0':
+#            second_digit = '1'
+#        else:
+#            second_digit = '0'
+#    new_pattern = first_digit
+#    for letter in pattern[1:]:
+#        if letter == pattern[0]:
+#            new_pattern += first_digit
+#        else:
+#            new_pattern += second_digit
+#    if sum([int(i) for i in new_pattern]) == 0 or sum([int(i) for i in new_pattern]) == len(pattern):
+#        return not_informative, 0
+#    if c_pattern == '':
+#        return new_pattern, 0
+#    dist = util.hamming_distance(c_pattern, new_pattern)
+#    if dist <= len(pattern)/2:
+#        return new_pattern, dist
+#    #print('new_pattern, dist',  new_pattern, dist)
+#    final_pattern = ''
+#    for letter in new_pattern:
+#        if letter == '0':
+#            final_pattern += '1'
+#        else:
+#            final_pattern += '0'
+#    return final_pattern, util.hamming_distance(c_pattern, final_pattern)
+#def test_informative():
+#    
+#    assert informative_pattern('A-CC') == ('', 0)
+#    assert informative_pattern('CCCC') == ('', 0)
+#    assert informative_pattern('AACC') == ('0011', 0)
+#    print(informative_pattern('AACC', '0011'))
+#    assert informative_pattern('AACC', '0011') == ('0011', 0)
+#    assert informative_pattern('AACC', '0010') == ('0011', 1)
+#    assert informative_pattern('CACC', '0011') == ('1011', 1)
+#    assert informative_pattern('ACCC', '1011') == ('1000', 2)
+#    assert informative_pattern('CCAC', '1110') == ('1101', 2)
 class Phase:
     def __init__(self, snp_id, mom_snp, sib_snp_list, mom_sibs='', mom_pat='', dad_sibs='', dad_pat=''):
         '''
@@ -97,43 +133,122 @@ def phase(mom, children):
         print(chrom)
         phase_chrom(chrom, [child.get_chromosome(chrom.get_name()) for child in children_genomes])
 def test_phase():
-    phase('/Users/jim/Documents/DNA/23andme-37-CarolynSmithTest2.txt', 
-          ['/Users/jim/Documents/DNA/23andme-37-CynthiaTest2.txt',
-            '/Users/jim/Documents/DNA/23andme-37-Suzanne_BusjahnTest2.txt',
-            '/Users/jim/Documents/DNA/23andmeJames_Smith-37-Test2.txt',
-            '/Users/jim/Documents/DNA/23andmeRay_Smith-37-Test2.txt']) 
+    phase('c:\\Users\\jim\\Documents\\DNA\\23andme-37-CarolynSmithTest2.txt', 
+          ['c:\\Users\\jim\\Documents\\DNA\\23andme-37-CynthiaTest2.txt',
+            'c:\\Users\\jim\\Documents\\DNA\\23andme-37-Suzanne_BusjahnTest2.txt',
+            'c:\\Users\\jim\\Documents\\DNA\\23andmeJames_Smith-37-Test2.txt',
+            'c:\\Users\\jim\\Documents\\DNA\\23andmeRay_Smith-37-Test2.txt'])     
+# for Mac
+#    phase('/Users/jim/Documents/DNA/23andme-37-CarolynSmithTest2.txt', 
+#          ['/Users/jim/Documents/DNA/23andme-37-CynthiaTest2.txt',
+#            '/Users/jim/Documents/DNA/23andme-37-Suzanne_BusjahnTest2.txt',
+#            '/Users/jim/Documents/DNA/23andmeJames_Smith-37-Test2.txt',
+#            '/Users/jim/Documents/DNA/23andmeRay_Smith-37-Test2.txt']) 
 
 def phase_chrom(chrome, children):
     '''
     chrome is the mother's (could be father's) chromosome as a util.Chromo object
     children is a list of Chrome objects of the children of the parent
+    return a Phase_chrom object containing the phased snps
     '''
+
+    p_chrom = util.Phase_chrom(chrome.get_name()) # create empty phased chromosome with just a name
+    snp_count = 0
+    include_snps = []
+    list_of_snp_names = chrome.get_snp_names()
+    m_start_pattern, d_start_pattern = look_ahead(chrome, children, 0)
+    c_m_inform = m_start_pattern
+    c_d_inform = d_start_pattern
+    p_chrom.set_m_start_pattern(m_start_pattern)
+    p_chrom.set_d_start_pattern(d_start_pattern)
+    print('m_start_pattern', m_start_pattern, 'd_start_pattern', d_start_pattern)
     print('snp_name,         mom,                 children, d_snps,   d_pat,  m_snps,    m_pat')
-    exclude_snps = []
-    for snp_name in chrome.get_snp_names():
-        
-        children_snps = [] 
-        for child in children:
-            #print(snp_name, child.get_snp(snp_name), child.get_name())
-            if child.get_snp(snp_name):
-                children_snps.append(child.get_snp(snp_name).get_uservariation())
-            else:
-                children_snps.append('--')
-            #children_snps += "."
-        if no_snps(children_snps): exclude_snps.append(snp_name)
+    for snp_index, snp_name in enumerate(chrome.get_snp_names()): # do this for each snp in mom's chromosome
+        children_snps = get_children_snps(children, snp_name)
+        if not no_snps(children_snps): include_snps.append(snp_name)  # only include the snp names where the children aren't all '--'
         mom_snp = chrome.get_snp(snp_name).get_uservariation()
-        p = pattern(mom_snp, children_snps)
-        if snp_name not in exclude_snps:
-            print(snp_name.ljust(12), mom_snp, children_snps, p[1], informative_pattern(p[1]).ljust(6), p[0], informative_pattern(p[0]).ljust(6))
+        if mom_snp == '--':
+            continue
+        #p = pattern(mom_snp, children_snps)
+        p_snp = util.Phased_snp(snp_name, mom_snp, children_snps, c_d_inform, c_m_inform) # make a phased snp
+        p_snp.phase(c_m_inform, c_d_inform)
+        if p_snp.get_d_dist() > 0:
+            p_chrom.add_crossovers(util.Crossover(snp_name, p_snp.get_d_dist(), p_snp.get_d_crossover(), 'dad'))    
+            c_d_inform = p_snp.get_d_pattern()
+        if p_snp.get_m_dist() > 0:
+            p_chrom.add_crossovers(util.Crossover(snp_name, p_snp.get_m_dist(), p_snp.get_m_crossover(), 'mom'))    
+            c_m_inform = p_snp.get_m_pattern()        
+        if snp_count < SNP_LIMIT and p_snp.get_name() in include_snps:
+            print(snp_name.ljust(12), mom_snp, children_snps, p_snp.get_f_dad(), p_snp.get_d_pattern().ljust(6), 
+                  p_snp.get_f_mom(), p_snp.get_m_pattern().ljust(6))
+        snp_count += 1
+    '''
+    At this point I've gone through the chromosome once. I've got a list of snps that can be calculated and,
+    the starting pattern, and the snp names where I detected a break.
+    '''
+    print('mom init informative', p_chrom.get_m_start_pattern(), ' init dad informative', p_chrom.get_d_start_pattern())
+    print('number of crossovers', len(p_chrom.get_crossovers()))
     #print('excluded: ', exclude_snps)
+    return p_chrom
+
+def look_ahead(chrome, children, snp_name_index):
+    '''
+    given a Phase_chrom object, an index into the snp names it contains and children, a list of Chrome objects for all of the children
+    scan ahead in the list of snps to find a complete informative patterns for mom and dad
+    return complete patterns if possible
+    
+    '''
+    m_start_pattern = ''
+    d_start_pattern = ''
+    snp_names = chrome.get_snp_names()
+    for time in ['first time', 'second time']:
+        for snp_index in range(snp_name_index, len(snp_names)):
+            snp_name = snp_names[snp_index]
+            children_snps = get_children_snps(children, snp_name)
+            #if not no_snps(children_snps): include_snps.append(snp_name)  # only include the snp names where the children aren't all '--'
+            mom_snp = chrome.get_snp(snp_name).get_uservariation()
+            #p = pattern(mom_snp, children_snps)
+            p_snp = util.Phased_snp(snp_name, mom_snp, children_snps) # make a phased snp
+            # first see check if dad's informative pattern has been set
+            if d_start_pattern == '':
+                if p_snp.get_d_pattern() != '':
+                    d_start_pattern = p_snp.get_d_pattern()
+                    break  # stop processing the chromosome and start over
+                continue # don't process further than this until dad's informative pattern has been found
+            # next look for mom's informative pattern
+            if m_start_pattern == '':
+                if p_snp.get_m_pattern() != '':
+                    m_start_pattern = p_snp.get_m_pattern()
+                    break # start over a second time, now with both mom and dad's starting informative pattern
+                continue # don't process further until mom's patter is found
+    return m_start_pattern, d_start_pattern
+
+def get_children_snps(children, snp_name):
+    '''
+    given a snp_name string like 'rs123', and children, a list of Chrom objects
+    put together the list of children snps for that name, ['AT', 'TT', ...]
+    '''
+    children_snps = [] 
+    for index, child in enumerate(children):
+        #print(snp_name, child.get_snp(snp_name), child.get_name())
+        if child.get_snp(snp_name):
+            children_snps.append(child.get_snp(snp_name).get_uservariation())
+        else:
+            children_snps.append('--')
+    return children_snps
+
 def no_snps(snp_list):
     for snp_str in snp_list:
         if snp_str != '--':
             return False
     return True
-
+def pattern_change(pattern1, pattern2):
+    if pattern1 == pattern2:
+        return False
+    else:
+        return True
 def run_tests():
-    test_informative()
+    #test_informative()
     test_phase()
     test_pattern()
     
--- util.py
+++ util.py
@@ -163,7 +163,263 @@ class Snp:
             #pair.append('-')
             pair += '-'
         return covered, pair
+
+class Phased_snp: 
+    def __init__(self, name, mom, children, m_pattern='', d_pattern=''): 
+        '''
+        name, rsid string; mom, list of strings ['AG', ..] for each kid; f_dad the letter gotten from dad
+        d_pattern, a pattern of inheritance to infer phasing, '0110'; f_mom the letter gotten from mom; m_pattern, string of mom's pattern
+        '''
+        self.name = name  # rs12...
+        self.mom = mom    # 'AG'
+        self.children = children # ['AG', 'AA', ...]
+        self.f_dad = ''
+        self.d_pattern = d_pattern
+        self.f_mom = ''
+        self.m_pattern = m_pattern
+        self.m_dist = 0  # if either mom or dad's pattern changes, record the distance here
+        self.d_dist = 0
+        self.d_crossover = 0 # the child that the crossover occurred
+        self.m_crossover = 0
+        self._first_phase(m_pattern, d_pattern)
+
+    def __str__(self):
+        return self.name.ljust(14) + self.mom.ljust(5) + '.'.join(self.children).ljust(15) + self.f_dad.ljust(9) + \
+                      self.d_pattern.ljust(8) + self.f_mom.ljust(9) + self.m_pattern.ljust(7) 
+    def get_d_pattern(self):
+        return self.d_pattern
+    def get_m_pattern(self):
+        return self.m_pattern
+    def get_d_dist(self):
+        return self.d_dist
+    def get_m_dist(self):
+        return self.m_dist
+    def get_m_crossover(self):
+        return self.m_crossover
+    def get_d_crossover(self):
+        return self.d_crossover
+    def get_f_dad(self):
+        return self.f_dad
+    def get_f_mom(self):
+        return self.f_mom
+    def get_name(self):
+        return self.name
+    def phase(self, m_pattern, d_pattern):
+        dad_snps = list(self.f_dad)
+        mom_snps = list(self.f_mom)
+        for times in range(2):
+            if '-' not in dad_snps and '-' not in mom_snps: break  # may need to loop zero or one times
+            d_zipped = zip(dad_snps, d_pattern)
+            for index, x in enumerate(d_zipped): # make list like [('-', '0'), ('T', '1'), (), ()]
+                if x[0] == '-':  # if the char is '-', look for a matching second char, '0' for example, that has a valid letter
+                    for z in d_zipped:
+                        if z[0] != '-' and z[1] == x[1]:
+                            dad_snps[index] = z[0]
+                            mom_snps[index] = self._other_letter(z[0], self.children[index])
+            m_zipped = zip(mom_snps, m_pattern)
+            for index, x in enumerate(m_zipped):
+                if x[0] == '-':
+                    for z in m_zipped:
+                        if z[0] != '-' and z[1] == x[1]:
+                            mom_snps[index] = z[0]
+                            dad_snps[index] = self._other_letter(z[0], self.children[index])
+        # now letters have been filled in. Maybe new patterns. Tricky to know what to do.
+        self.f_mom = ''.join(mom_snps)
+        self.f_dad = ''.join(dad_snps)
+        self.m_pattern, self.m_dist, self.m_crossover = self._informative_pattern(self.f_mom, m_pattern)
+        self.d_pattern, self.d_dist, self.d_crossover = self._informative_pattern(self.f_dad, d_pattern)
         
+    def _first_phase(self, m_pattern='', d_pattern=''):
+        self.f_mom, self.f_dad = self._pattern(self.mom, self.children)  # gets patterns like from mom '-TT-' and from dad 'CCCC'
+        if d_pattern == '':
+            self.d_pattern, self.d_dist, self.d_crossover = self._informative_pattern(self.f_dad)
+        else:
+            self.d_pattern, self.d_dist, self.d_crossover = self._informative_pattern(self.f_dad, d_pattern)
+        if m_pattern == '':
+            self.m_pattern, self.m_dist, self.m_crossover = self._informative_pattern(self.f_mom)
+        else:
+            self.m_pattern, self.m_dist, self.m_crossover = self._informative_pattern(self.f_mom, self.m_pattern)  
+    def _other_letter(self, letter, snp):
+        '''
+        letter is A, T, G, or C; snp is AG, GC, GG, ...
+        return the other letter, could actually be the same letter
+        '''
+        if letter == snp[0]:
+            return snp[1]
+        else:
+            return snp[0]
+    def _pattern(self, mom_snp, sib_list):
+        '''
+        mom_snp is a two letter string like 'AC'
+        sib_list is a list of two letter strings like ['AC', ...]
+        return two values, mom_pattern, dad_pattern
+        the patterns are either None, or a string with the same number of letters as there are sibs
+        '''
+        mom_pattern = ['-'] * len(sib_list) # both mom and dad start with as many dashes as are siblings
+        dad_pattern = mom_pattern[:]
+        for index, sib in enumerate(sib_list):
+            if sib[0] == sib[1]:
+                dad_pattern[index] = sib[0] # homozygous, got the same letter from both parents
+                if sib[0] not in mom_snp:
+                    continue
+                mom_pattern[index] = sib[0]
+                continue
+            if mom_snp[0] == mom_snp[1]:
+                mom_pattern[index] = mom_snp[0] # if mom is homozygous, then the kid got that letter from mom
+                if sib[0] != mom_snp[0] and sib[0] != '-':
+                     # mom CC, sib CA, so C came from mom, A from Dad
+                    dad_pattern[index] = sib[0]
+                if sib[1] not in mom_snp and sib[1] != '-':
+                    dad_pattern[index] = sib[1]
+                continue
+            if mom_snp == sib: # mom and sib are both AC for instance, can't tell anything
+                continue
+            if sib[0] in mom_snp and sib[1] not in mom_snp:
+                mom_pattern[index] = sib[0]
+                dad_pattern[index] = sib[1]
+                continue
+            if sib[1] in mom_snp and sib[0] not in mom_snp:
+                mom_pattern[index] = sib[1]
+                dad_pattern[index] = sib[0]
+                continue
+            #print('error, one of the above cases should have happened, mom_snp, sib', mom_snp, sib)
+        return ''.join(mom_pattern), ''.join(dad_pattern)
+
+    def _informative_pattern(self, pattern, c_pattern=''):
+        '''
+        take a pattern, and return empty string if not informative, or full string if informative
+        c_pattern is the current pattern, like '0111' or '1100' or '' if there hasn't been a pattern yet
+        informative are like 'AACC' or 'TCCC'
+        there should only be two letters, but I don't test
+        return '' if no informative pattern, or '0111' or some such informative pattern plus a Hamming distance and child
+        if the Hamming distance is 0 or 1, all is well. If greater, then there's a 50/50 chance the resulting pattern is wrong
+        child is the first position that has a change and is a number from 0 to n-1
+        '''
+        not_informative = ''
+        if '-' in pattern:
+            return not_informative, 0, 0
+        if c_pattern == '':
+            first_digit = '0'
+            second_digit = '1'
+        else:
+            first_digit = c_pattern[0]
+            if first_digit == '0':
+                second_digit = '1'
+            else:
+                second_digit = '0'
+        new_pattern = first_digit
+        for letter in pattern[1:]:
+            if letter == pattern[0]:
+                new_pattern += first_digit
+            else:
+                new_pattern += second_digit
+        if sum([int(i) for i in new_pattern]) == 0 or sum([int(i) for i in new_pattern]) == len(pattern):
+            return not_informative, 0, 0
+        if c_pattern == '':
+            return new_pattern, 0, 0
+        dist, child = hamming_distance(c_pattern, new_pattern)
+        if dist <= len(pattern)/2:
+            return new_pattern, dist, child
+        #print('new_pattern, dist',  new_pattern, dist)
+        final_pattern = ''
+        for letter in new_pattern:
+            if letter == '0':
+                final_pattern += '1'
+            else:
+                final_pattern += '0'
+        return final_pattern, hamming_distance(c_pattern, final_pattern)[0], hamming_distance(c_pattern, final_pattern)[1]
+def test_phase():
+    snp2 = Phased_snp('rs2', 'AC', ['AC', 'AA', 'AA', 'CC'])
+    
+def test_informative():
+    snp2 = Phased_snp('rs2', 'AC', ['AC', 'AA', 'AA', 'CC'])
+    assert snp2._informative_pattern('A-CC') == ('', 0, 0)
+    assert snp2._informative_pattern('CCCC') == ('', 0, 0)
+    assert snp2._informative_pattern('AACC') == ('0011', 0, 0)
+    assert snp2._informative_pattern('AACC', '0011') == ('0011', 0, 0)
+    assert snp2._informative_pattern('AACC', '0010') == ('0011', 1, 3)
+    assert snp2._informative_pattern('CACC', '0011') == ('1011', 1, 0)
+    assert snp2._informative_pattern('ACCC', '1011') == ('1000', 2, 2)
+    assert snp2._informative_pattern('CCAC', '1110') == ('1101', 2, 2)
+
+def test_Phased_snp():
+    print('snp_name,   mom,     children, d_snps,   d_pat,  m_snps,    m_pat')
+    snp2 = Phased_snp('rs2', 'AA', ['AA', 'AA', 'AG', '-G'])
+    assert snp2.get_f_dad() == 'AAGG' and snp2.get_f_mom() == 'AAAA'
+    #print(snp2)
+    snp3 = Phased_snp('rs3', 'CT', ['CT', 'TT', 'TT', 'CT'], '', '0011')
+    #
+    assert snp3.get_f_dad() == '-TT-' and snp3.get_f_mom() == '-TT-'
+    snp3.phase('', '0011')
+    print(snp3)
+    #assert snp3.get_m_pattern() == '0110'
+    snp16 = Phased_snp('rs16', 'CC', ['CT', 'CC', 'CC', 'CC'])
+    #print(snp16)
+    #assert snp16.get_d_pattern() == '0111'
+    snp19 = Phased_snp('rs19', 'GT', ['GT', 'GT', 'GT', 'GG'])
+    #print(snp19)
+    snp19.phase('0110', '0111') 
+    #print(snp19) 
+    #assert snp19.get_d_pattern() == '0111' and snp19.get_m_pattern() == '0110'
+    
+    snpx = Phased_snp('rsx', 'GG', ['GT', 'GT', 'GG', 'GG'], '', '1000')
+    #print(snpx)
+    #print(snpx.get_d_dist(), snpx.get_m_dist(), snpx.get_d_crossover(), snpx.get_m_crossover())
+    #assert snpx.get_d_dist() == 1 and snpx.get_d_crossover() == 1
+    snprs914994 = Phased_snp('rs914994', 'GT', ['GG', 'GT', 'GT', 'GT'], '0001', '1001')
+    #print(snprs914994)
+    snprs914994.phase('0001', '1001')
+    #assert snprs914994.get_m_pattern() == '0001' and snprs914994.get_d_pattern() == '1001'
+    
+class Phase_chrom:
+    '''
+    phased chromosome for a family, a mom, children, holds the estimate for the father
+    '''
+    def __init__(self, name, phased_snps = [], m_start_pattern = '', d_start_pattern = ''):
+        self.name = name  # '1', 'MT', 'X', ...
+        self.phased_snps = phased_snps # a list of phased snps
+        self.m_start_pattern =  m_start_pattern # the chromosome phasing pattern at the start of the chromosome, '0011' for mom
+        self.d_start_pattern = d_start_pattern
+        self.crossovers = [] # list of crossover objects
+    def __str__(self):
+        return 'name ' + self.name + ', number snps ' + str(len(self.phased_snps)) + ', number of crossovers ' + str(len(self.crossovers)) + \
+            ', mom start pattern ' + self.m_start_pattern + ', dad start pattern ' + self.d_start_pattern
+    def set_m_start_pattern(self, m_start_pattern):
+        self.m_start_pattern = m_start_pattern
+    def get_m_start_pattern(self):
+        return self.m_start_pattern
+    def set_d_start_pattern(self, d_start_pattern):
+        self.d_start_pattern = d_start_pattern
+    def get_d_start_pattern(self):
+        return self.d_start_pattern
+    def get_name(self):
+        return self.name
+    def add_snp(self, snp):
+        self.phased_snps.append(snp)
+    def get_snps(self):
+        return self.phased_snps
+    def add_crossovers(self, crossover):
+        self.crossovers.append(crossover)
+    def get_crossovers(self):
+        return self.crossovers
+              
+class Crossover:
+    '''
+    when a crossover is found store relavant information here
+    '''
+    def __init__(self, snp_name, h_dist, child, parent):
+        self.snp_name = snp_name # the name of the snp that the crossover was detected. the actual crossover will be earlier
+        self.h_dist = h_dist  # hamming distance normally 1. if 2 or greater there will be a problem with phasing
+        self.child = child    # 0 or 1 or ...
+        self.parent = parent  # 0 or 1, mom or dad
+    def __str__(self):
+        return 'snp_name ' + self.snp_name + ', hamming distance ' + self.h_dist + ', child ' + self.child + ', parent ' + self.parent
+def test_pattern():
+    p = Phased_snp('name', 'CA', ['CC', 'AC', 'CT', 'TT'])
+    assert p._pattern('CA', ['CC', 'AC', 'CT', 'TT']) == ('C-C-', 'C-TT')
+    #assert Phased_snp._pattern('CA', ['CC', 'AC', 'CT', 'TT']) == ('C-C-', 'C-TT')
+    #assert Phased_snp._pattern('CC', ['CC', 'AC', 'CT', 'TT']) == ('CCC-', 'CATT')   
+
 def parseFTDna(filein):
     allSnps = []
     for line in filein:
@@ -319,8 +575,23 @@ def save_object(obj, filename):
 def get_object(filename):
     with open(filename, 'rb') as input:
         return pickle.load(input)
-     
+def hamming_distance(s1, s2):
+    """
+    Return the Hamming distance between equal-length sequences and first position with a mismatch
+    if no mismatch, 0, 0; 
+    """
+    if len(s1) != len(s2):
+        raise ValueError("Undefined for sequences of unequal length")
+    dist = sum(ch1 != ch2 for ch1, ch2 in zip(s1, s2)) 
+    if dist == 0:
+        return 0, 0
+    for i in range(len(s1)):
+        if s1[i] != s2[i]:
+            return dist, i
 def runtests():
+    test_Phased_snp()
+    test_pattern()
+    test_informative()
     #test_parse_files()
     #test_compute_father()
     #test_parseFTDna()
